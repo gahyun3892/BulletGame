5d2076925f146f418080f7a5d0e7d55c9d0d2442
gahyun3892 <66283620+gahyun3892@users.noreply.github.com>
2020-06-03 10:31:26 +0900

    30307박가현

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("앞으로~");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z + 3f * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("뒤로~");
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                transform.position.z - 3f * Time.deltaTime);
+        }
+    }
+}
